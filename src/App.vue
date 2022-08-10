3277fcaf12ec198ab1a6692b80ca1a29ec2931c7
Pooya Dehghani <pooya.dehghani@hm.com>
2022-08-10 16:35:26 +0200

    Add tasks component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,60 @@
 <template>
   <div class="container">
     <Header heading="Task Tracker" />
+    <Tasks
+      :tasks="tasks"
+      @delete-task="deleteTask"
+      @toggle-reminder="toggleReminder"
+    />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 export default {
   name: "App",
   components: {
     Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: "1",
+        text: "Doctors Appointment",
+        day: "March 5th at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: "2",
+        text: "Meeting with boss",
+        day: "March 6th at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: "3",
+        text: "Food shopping",
+        day: "March 7th at 2:00pm",
+        reminder: false,
+      },
+    ];
   },
 };
 </script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <Task
+      :task="task"
+      @delete-task="$emit('delete-task', task.id)"
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+    />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+export default {
+  name: "Tasks",
+  props: { tasks: Array },
+  emits: ["delete-task", "toggle-reminder"],
+  components: { Task },
+};
+</script>
